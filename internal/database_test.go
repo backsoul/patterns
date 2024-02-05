dce688a395383f882b7b8659f2f92f728488c8ed
Daniel Sarmiento <danielsarmientoenterprise@gmail.com>
2024-02-04 22:56:35 -0500

    changes and commit

--- internal/database_test.go
+++ internal/database_test.go
@@ -13,9 +13,7 @@ type MockDatabaseConnector struct {
 	DB *sql.DB
 }
 
-// Connect simula la conexión a la base de datos.
 func (m *MockDatabaseConnector) Connect() (*sql.DB, error) {
-	// Crear un DB real y un sqlmock para pruebas.
 	db, _, err := sqlmock.New()
 	if err != nil {
 		return nil, err
@@ -23,15 +21,10 @@ func (m *MockDatabaseConnector) Connect() (*sql.DB, error) {
 
 	m.DB = db
 
-	// Configurar el mock según sea necesario para tus pruebas.
-	// Por ejemplo, mock.ExpectQuery("SELECT NOW()").WillReturnRows(sqlmock.NewRows([]string{"now"}).AddRow("2022-02-04T12:00:00Z"))
-
 	return db, nil
 }
 
-// GetNow simula la obtención de la fecha y hora actual.
 func (m *MockDatabaseConnector) GetNow() (string, error) {
-	// Puedes usar el DB real o el sqlmock aquí, según tus necesidades.
 	rows := m.DB.QueryRow("SELECT NOW()")
 	var now string
 	if err := rows.Scan(&now); err != nil {
@@ -41,14 +34,10 @@ func (m *MockDatabaseConnector) GetNow() (string, error) {
 }
 
 func TestDatabaseIntegration(t *testing.T) {
-	// Configurar el mock para que devuelva los resultados esperados.
-
 	var dbConnector pkg.Database = &MockDatabaseConnector{}
 	db, err := dbConnector.Connect()
 	if err != nil {
 		t.Fatal("Failed to connect to the database:", err)
 	}
 	defer db.Close()
-
-	// Resto del código de tu prueba...
 }
